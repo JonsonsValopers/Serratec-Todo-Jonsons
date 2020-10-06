8c86cf08b424ba00f673b502c8972bd8f8bcb29e
BrGUILHERMEHenrique <guilhermehenriquersouza17@gmail.com>
2020-10-06 09:57:54 -0300

    Bottom-tabs nvigation, cadastro

---
+++ src/pages/Cadastro/index.js
@@ -0,0 +1,8 @@
+import React, { useEffect, useState, useCallback } from 'react';
+
+import { Text, Button, ButtonText, Link } from './styles';
+
+const Cadastro = () => {
+}
+
+export default Cadastro;
--- src/service/api.js
+++ src/service/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
     // baseURL: 'http://10.0.2.2:3333'
     // baseURL: 'http://192.168.1.4:3333'//IP da maquina de quem vai s=usar assim
-    baseURL: ''//ngrok (Guilherme)
+    baseURL: 'http://386e89ed9e36.ngrok.io'//ngrok (Guilherme)
 });
 
 export default api;
