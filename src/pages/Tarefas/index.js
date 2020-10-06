6b519a752a6e0fe15223e7197655855a88ea145f
BrGUILHERMEHenrique <guilhermehenriquersouza17@gmail.com>
2020-10-06 15:27:55 -0300

    Criando: página tarefas

---
+++ src/pages/Tarefas/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native'
+
+const Tarefas = () => {
+    return(
+        <Text>Olá Minhas tarefas</Text>
+    )
+}
+
+export default Tarefas;
