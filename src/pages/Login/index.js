47aecebe5a2d10cef29eb82042739732532aa5b7
lucca martinelli <lucnjm@gmail.com>
2020-10-06 15:13:58 -0300

    login - busca api, e linkado para cadastro

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,8 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useCallback, useEffect } from 'react'
 import { ActivityIndicator } from 'react-native';
 import { useAuth } from '../../hooks/auth';
+import { useNavigation } from '@react-navigation/native';
+
 
 import logo from '../../assets/Logo.png';
+import api from '../../services/api';
+import Cadastro from '../Cadastro'
 
 import { Container, InputLogin, ButtonSubmit, ButtonSignup, ButtonText, Imagem } from './styles';
 
@@ -12,6 +16,21 @@ const Login = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [carregando, setCarregando] = useState(false);
+    const [buscaUsuarios, setBuscaUsuarios] = useState([]);
+    const navigation = useNavigation();
+
+    const loadUsuarios = useCallback(
+        async () => {
+            const resposta = await api.get('/usuarios');
+            // console.log(resposta.data);
+            setBuscaUsuarios(resposta.data);
+
+        }, [],
+    );
+    useEffect(() => {
+        loadUsuarios();
+    }, [loadUsuarios]);
+
 
 
     return (
@@ -44,7 +63,7 @@ const Login = () => {
                 {carregando ? (
                     <ActivityIndicator color="#fff" />
                 ) : (
-                        <ButtonText>Cadastre-se</ButtonText>
+                        <ButtonText onPress={() => navigation.navigate(Cadastro)}>Cadastre-se</ButtonText>
                     )}
             </ButtonSignup>
 
