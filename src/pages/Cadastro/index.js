c427ef31059ea4ba8233da8891656242369659b9
BrGUILHERMEHenrique <guilhermehenriquersouza17@gmail.com>
2020-10-06 10:51:50 -0300

    RegExp email e senha

--- src/pages/Cadastro/index.js
+++ src/pages/Cadastro/index.js
@@ -1,8 +1,74 @@
 import React, { useEffect, useState, useCallback } from 'react';
+import { useNavigation } from '@react-navigation/native';
 
-import { Text, Button, ButtonText, Link } from './styles';
+import api from '../../service/api';
+import { useAuth } from '../../hooks/auth';
+import logoImg from '../../assets/Logo.png';
+
+import { Container, Title, Input, Button, ButtonText, Link, Image } from './styles';
 
 const Cadastro = () => {
+    const { signUp } = useAuth();
+    const navigation = useNavigation();
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+    const [erroCampo, setErroCampo] = useState('');
+
+    const cadastro = async () => {
+        try {
+            signUp(email, senha);
+            console.log('cadastro sucess');
+            setEmail('');
+            setSenha('');
+        } catch (error) {
+            console.log('cadastro: ', error);
+        }
+    }
+
+    // const validar = () =>  {
+
+    //     if(email == "" || senha == "") {
+    //         setErroCampo("Campo preenchido incorretamente!");
+    //     }
+        
+    //     let regexEmail = /^|([\w\d^\s]{2,}|)[@][a-z0-9]{2,10}[.][a-z]{3,6}$/g;
+
+    //     let regexSenha = /^[a-z0-9\w\d^\s]{8,20}$/g;
+
+    //     if(!regexEmail.test(email) || !regexSenha.test(senha)) {
+    //         setErroCampo("Campo preenchido incorretamente!");
+    //     }
+        
+    // }
+
+
+
+    return(
+        <Container>
+            <Image source={logoImg} /> 
+            <Title> Cadastro </Title>
+            <Input 
+            value={email}
+            onChangeText={text => setEmail(text)}
+            placeholder="E-mail"
+            />
+            <Input 
+            value={senha}
+            onChangeText={text => setSenha(text)}
+            placeholder="E-mail"
+            />
+            <Button
+                onPress={() => cadastro()}
+            >
+                <ButtonText>Cadastrar</ButtonText>
+            </Button>
+
+            <Link
+                onPress={() => navigation.navigate(Login)}
+            >Faça o seu Login</Link>
+        </Container>
+    
+    )
 }
 
 export default Cadastro;
--- src/pages/Cadastro/styles.js
+++ src/pages/Cadastro/styles.js
@@ -1,1 +1,53 @@
 import styled from 'styled-components/native';
+
+const Container = styled.view`
+    padding: 20px;
+    flex: 1;
+    background-color: #fff;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const Image = styled.Image` 
+    margin-top: 30px;
+    width: 300px;
+    height: 300px;
+`;
+
+export const Title = styled.Text`
+    margin-top: 20px;
+    font-size: 30px;
+    font-weight: bold;
+    color: #3a3a3a;
+    
+`;
+
+export const Input = styled.TextInput`
+    width: 300px;
+    height: 40px;
+    background-color: #fff;
+    color: #3a3a3a;
+    border-radius: 5px;
+
+`;
+
+export const Button = styled.TouchableOpacity`
+    width: 200px;
+    height: 40px;
+    background-color: #3a5b3a;
+    border-radius: 5px;
+`;
+
+export const ButtonText = styled.Text`
+    font-size: 20px;
+    font-weight: bold;
+    color: #fff;
+
+`;
+
+export const Link = styled.Text`    
+    margin-top: 20px;
+    font-size: 20px;
+    font-weight: 200;
+    color: blue;
+`;
