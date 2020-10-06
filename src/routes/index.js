abedaad39655e662eade9f0220af284649bc247a
cicero romao <cicerotransformice@gmail.com>
2020-10-06 10:25:14 -0300

    Subindo Os Routes e A pagina de Login

---
+++ src/routes/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { createStackNavigator } from 'react-navigation/stack'
+
+import  AppRoutes from '../routes/app.routes';
+import  AuthRoutes from '../routes/auth.routes';
+
+import { useAuth } from '../hooks/auth';
+
+const Stack = createStackNavigator();
+
+const RootNavigator = () => {
+    const { user } = useAuth();
+
+    return (
+        <Stack.Navigator>      
+        <Stack.Screen name="Root" component={ user ? AppRoutes : AuthRoutes } />
+        </Stack.Navigator>
+    )
+}
