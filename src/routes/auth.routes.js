55739aa4943ced67c385c731c3cc92b6b912a29c
cicero romao <cicerotransformice@gmail.com>
2020-10-06 10:55:12 -0300

    Subindo Routes auth.routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Login from '../pages/Login';
+import Cadastro from '../pages/Cadastro';
 
 const Auth = createStackNavigator();
 
@@ -10,6 +11,7 @@ const AuthRoutes = () => {
     return (
         <Auth.Navigator screenOptions={{ headerShown: false }}>
             <Auth.Screen name="Login" component={Login}/>
+            <Auth.Screen name="Cadastro" component={Cadastro}/>
         </Auth.Navigator>
     )
 }
