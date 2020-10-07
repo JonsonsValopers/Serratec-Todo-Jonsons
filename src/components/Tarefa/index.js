04145432f13177bef3c4ef8dc931a7855960e475
BrGUILHERMEHenrique <guilhermehenriquersouza17@gmail.com>
2020-10-07 17:23:47 -0300

    Criando: busca de projeto

--- src/components/Tarefa/index.js
+++ src/components/Tarefa/index.js
@@ -2,13 +2,14 @@ import React, { useState } from 'react';
 
 import  {Picker } from '@react-native-community/picker';
 
-import { Tarefa as Container } from './styles';
+import { Tarefa as Container, TituloTarefa, NomeProjeto, BotaoExcluir, BotaoTexto, Botoes  } from './styles';
 import BotaoConcluido from '../../components/BotaoConcluido';
 import api from '../../services/api'
 
 const Tarefa = (props) => {
-    const { tarefa, usuarios } = props;
+    const { tarefa, usuarios, projeto } = props;
     const [usuario, setUsuario] = useState({});
+    const [projeto, setProjeto] = useState({});
 
     const atualizarUsuario = async (usuario) => {
         tarefa.usuarioId = usuario.id;
@@ -18,28 +19,66 @@ const Tarefa = (props) => {
         } catch (error) {
             console.log(error);
         }
-    }   
+    }
+
+    const pegarProjeto = async (usuario) => {
+        tarefa.usuarioId = usuario.id;
+        try {
+            const resposta = await api.get(`projetos/${tarefa.projetoId}`);
+            setProjeto(resposta.data);
+            console.log(projeto.data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(
+        () => {
+            pegarProjeto();
+        }, [pegarProjeto]
+    )
+    
+    
 
     return(
-        <>
+        <Container>
 
-        <Picker
-            selectedValue={usuario}
-            style={{height: 50, width: 100}}
-            onValueChange={(itemValue, itemIndex) => {
-                setUsuario(itemValue);
-                atualizarUsuario(itemValue);
-            }
-            }>
+            <TituloTarefa>{tarefa.descricao}</TituloTarefa>
+
+            <NomeProjeto>{projeto.nome}</NomeProjeto>
+
+        <Botoes>
+            <BotaoConcluido tarefa={tarefa} />
+
+            <Picker
+                selectedValue={usuario}
+                style={{height: 50, width: 100}}
+                onValueChange={(itemValue, itemIndex) => {
+                    setUsuario(itemValue);
+                    atualizarUsuario(itemValue);
+                }
+                }>
+                {
+                    usuarios.map(usuario => (
+                        <Picker.Item label={usuario.email} value={usuario} />
+                    ))
+                }
+            </Picker>
+
             {
-                usuarios.map(usuario => (
-                    <Picker.Item label={usuario.email} value={usuario} />
-                ))
+                tarefa.concluido ? 
+                (<BotaoExcluir onPress={() => excluirTarefa()}>
+                    <BotaoTexto>Excluir</BotaoTexto>
+                    </BotaoExcluir>
+                    ) : 
+                    (<BotaoExcluir disabled={true} >
+                        <BotaoTexto>Excluir</BotaoTexto>
+                    </BotaoExcluir>
+                    )
             }
-        </Picker>
+        </Botoes>
 
-        <BotaoConcluido tarefa={tarefa} />
-        </>
+        </Container>
     )
 }
 
--- src/components/Tarefa/styles.js
+++ src/components/Tarefa/styles.js
@@ -6,21 +6,23 @@ export const Tarefa = styled.View`
 `;
 
 export const TituloTarefa = styled.Text`
-    
+    font-size: 20px;
+    font-weight: 400;
 `;
 
-export const BotaoAtualizar = styled.TouchableOpacity`
-    background-color: transparent;
-    width: 40px;
-    height: 40px;
+export const NomeProjeto = styled.Text`
+    font-size: 20px;
+    font-weight: bold;
 `;
 
-export const BotaoMudarUsuario = styled.TouchableOpacity`
-    background-color: blue;
-    width: 40px;
-    height: 40px;
-`;
 
+export const Botoes = styled.View`
+
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+
+`;
 
 export const BotaoExcluir = styled.TouchableOpacity`
     background-color: red;
