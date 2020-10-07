bece62a82ddba45a2951366278fe1ddad6f2940f
PauloEduardoLucas <dudu.magnificat@gmail.com>
2020-10-07 09:55:39 -0300

    subindo validações com regex comentadas para testes futuros

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333'
+    // baseURL: 'http://localhost:3333'
     // baseURL: 'http://192.168.1.4:3333'//IP da maquina de quem vai s=usar assim
     // baseURL: 'http://386e89ed9e36.ngrok.io'//ngrok (Guilherme)
+    baseURL: 'https://192.168.0.80:3333'
 });
 
 export default api;
+
+// json-server --watch db.json --host 192.168.0.80 --port 3333 (do paulo nao meche kkk)
