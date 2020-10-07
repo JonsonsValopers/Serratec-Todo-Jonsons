e1b466fc0be41fb15c4a85814381a9df09dbd294
Gabriela Oliveira <eradigitalaquarius@gmail.com>
2020-10-07 11:45:06 -0300

    importando styled

--- src/pages/ProjetosDetalhes/styles.js
+++ src/pages/ProjetosDetalhes/styles.js
@@ -1,4 +1,13 @@
 import styled from 'styled-components/native'
 
+export const Container = styled.View`
+`;
+
 export const Tarefas = styled.View`
+`;
+
+export const Tarefa = styled.View`
+`;
+
+export const TarefaDescricao = styled.View`
 `;
