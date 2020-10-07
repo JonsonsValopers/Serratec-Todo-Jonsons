ae0dafd208806c468e587ea0e85c053ce5814af1
cicero romao <cicerotransformice@gmail.com>
2020-10-07 11:32:21 -0300

    subindo alteracoes das paginas dos Projetos e estilizacoes do mesmo

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // baseURL: 'http://localhost:3333'
+    baseURL: 'http://localhost:3333'
     // baseURL: 'http://192.168.1.4:3333'//IP da maquina de quem vai s=usar assim
     // baseURL: 'http://386e89ed9e36.ngrok.io'//ngrok (Guilherme)
-    baseURL: 'https://192.168.0.80:3333'
+    // baseURL: 'https://192.168.0.80:3333'
 });
 
 export default api;
