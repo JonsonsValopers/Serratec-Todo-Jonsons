1bc96912ddf7a86f44cd6b64437c8a1578a6e2ba
BrGUILHERMEHenrique <guilhermehenriquersouza17@gmail.com>
2020-10-08 13:32:08 -0300

    Rota ngrok

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
     // baseURL: 'http://localhost:3333'
     // baseURL: 'http://192.168.1.4:3333'//IP da maquina de quem vai usar assim
-    baseURL: 'http://cb6ec3158c43.ngrok.io'//ngrok (Guilherme)
+    baseURL: 'http://7955c30a7c93.ngrok.io'//ngrok (Guilherme)
 });
 
 export default api;
